7effeab03fba03d5aa5fcebe46d1ef9ce2b0b36f
winklesen <samuelbernard147@gmail.com>
2024-03-21 14:56:00 +0700

    :zap: Reinit project from existing backup

--- src/id/winklesen/learnKotlin/Main.kt
+++ src/id/winklesen/learnKotlin/Main.kt
@@ -1,4 +1,18 @@
-package com.samuelbernard.kotlinfundamental.basic
+package id.winklesen.learnKotlin////TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+//// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//fun main() {
+//    val name = "Kotlin"
+//    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+//    // to see how IntelliJ IDEA suggests fixing it.
+//    println("Hello, " + name + "!")
+//
+//    for (i in 1..5) {
+//        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+//        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+//        println("i = $i")
+//    }
+//}
+
 
 // Single line comment
 /*
@@ -23,7 +37,7 @@ fun main(args: Array<String>) {
 }
 
 fun tesKata(kata:String): String {
- return kata
+    return kata
 }
 
 fun testKataVoid(kata: String){
