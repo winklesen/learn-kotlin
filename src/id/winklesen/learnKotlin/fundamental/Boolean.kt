223deb8af8ee57716e5b96cf26f988eb68cdd2c7
winklesen <samuelbernard147@gmail.com>
2024-03-22 11:45:25 +0700

    :zap: Add Fundamental

---
+++ src/id/winklesen/learnKotlin/fundamental/Boolean.kt
@@ -0,0 +1,31 @@
+package id.winklesen.learnKotlin.fundamental
+
+fun main() {
+    // boolean
+    val officeOpen = 7
+    val officeClosed = 16
+    val now = 20
+
+    // (operator AND &&)
+    val isOpen = if (now >= officeOpen && now <= officeClosed){
+        true
+    } else {
+        false
+    }
+
+    println("Office is open : $isOpen")
+
+    // (operator OR ||)
+    val isClose = now < officeOpen || now > officeClosed
+
+    println("Office is closed : $isClose")
+
+    // (operator NOT !)
+    if (!isOpen) {
+        print("Office is closed")
+    } else {
+        print("Office is open")
+    }
+
+
+}
